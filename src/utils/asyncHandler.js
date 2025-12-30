125cb0bd293a4f50a32e7d422555144612822577
bhuvan-365 <bhuvanbhattarai07@gmail.com>
2025-12-30 13:46:25 +0545

    usermodule created

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,19 +1,22 @@
-// const asyncHandler = ()=>{
 
-// }
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
 
-export {asyncHandler}
+export { asyncHandler }
 
+/* USING ASYNC HANDLER TO WRAP ASYNC CONTROLLERS TO CATCH ERRORS
 const asyncHandler = (fn) => async (req,res,next) => {
-
     try{ 
+        await fn(req,res,next)
 
     } catch(err){
-     
         res.status(err.code || 500).json({
             sucess:false,
             message:err.message || "Internal Server Error"
         })
-
     }
 }
+    */
