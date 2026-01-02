95fe4392bac37a648f552799509b4b1661c12db4
bhuvan-365 <bhuvanbhattarai07@gmail.com>
2026-01-02 16:41:03 +0545

    SET UP  Configure

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+ 
 const app = express();
 
 app.use(cors({
@@ -13,5 +13,11 @@ app.use(express.json({limit:"16kb"})); // for parsing application/json size
 app.use(express.urlencoded({extended:true, limit:"16kb"})); // for parsing application/x-www-form-urlencoded
 app.use(express.static("public")) // to serve static files from the "public" directory
 app.use(cookieParser()); // to parse cookies from the request headers
+
+//routes
+import userRouter from "./routes/user.routes.js";
+
+//route declarations
+app.use("/api/v1/users", userRouter);
 
 export default app;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,6 @@ const router = Router();
 
 
 router.route("/register").post(registerUser);
-router.route("/login").post(login); // to be implemented
+// router.route("/login").post(login);
 
 export default router;
