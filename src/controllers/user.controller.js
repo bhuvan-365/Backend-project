56011db3e2a600d185f294b660d29f146fb4a6a7
bhuvan-365 <bhuvanbhattarai07@gmail.com>
2026-01-07 14:45:17 +0545

    set acesstoken and refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -132,11 +132,33 @@ const loginUser = asyncHandler(async (req, res) => {
     })
 
  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
+const loggedInUser = await User.findById(user._id).select("-password -refreshTokens")
+ 
+const options ={
+    httpOnly:true,
+    secure:true,
+}
+return res
+.status(200)
+.cookie("accessToken", accessToken, options)
+.cookie("refreshToken", refreshToken, options)
+.json(
+    new ApiResponse(200,
+         {
+             user: loggedInUser, accessToken, refreshToken 
+            
+            }, "User logged in successfully")
+)
 
+})
+
+const logoutUser = asyncHandler(async(req,res)=>{
+    
 })
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
 
