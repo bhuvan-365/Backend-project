b5d520bda9770eed5d61f17f2dcd467583acb240
bhuvan-365 <bhuvanbhattarai07@gmail.com>
2026-01-11 18:20:26 +0545

    aggregation pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -385,10 +385,35 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
                     }
                 }
             }
+        },
+        {
+            $project:{
+                fullName:1,
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1,
+                createdAt:1,
+
+            }
         }
 
 
    ])
+   console.log(channel)
+
+if(!channel?.length){
+    throw new ApiError(400, "channel does not exist")
+}
+return res
+.status(200)
+.json(
+    new ApiResponse(200,channel[0],"User channel fetched sucessfully")
+)
+
 })
 
 export {
